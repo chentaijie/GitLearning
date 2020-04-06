d346fd608d8ea384ff4bbcafed3387af713b1225
chentaijie <504495889@qq.com>
2020-04-06 16:37:45 +0800

    update aaron.cpp

--- aaron.cpp
+++ aaron.cpp
@@ -3,10 +3,6 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
-    cout << "hello, arron." << endl;
-=======
     cout << "hello, chentaijie." << endl;
->>>>>>> bce37ee4743ec513d4bdcc22e17206676ce3668d
     return 0;
 }
